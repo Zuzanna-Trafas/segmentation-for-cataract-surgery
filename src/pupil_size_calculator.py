e7b05f166ced7bdd2518b6d62abaa61aef646b9f
Dominika Darabos <dominika_darabos@vulcan.ifl.campar.in.tum.de>
2024-01-12 16:11:51 +0100

    Change the contour selection logic, add special sorting for os.listdir and small cleaning

--- src/pupil_size_calculator.py
+++ src/pupil_size_calculator.py
@@ -1,6 +1,7 @@
 import cv2
 import numpy as np
 import os
+import re
 
 class PupilSizeCalculator:
      def __init__(self, threshold):
@@ -12,6 +13,35 @@ class PupilSizeCalculator:
           self.previous_file_path = None
           self.change_counter = 0
      
+
+     ######### Getters and setters ######### 
+
+     def reset_previous_pupil_size(self):
+          self.previous_pupil_size = None
+     
+     def set_previous_pupil_size(self, size):
+          self.previous_pupil_size = size
+     
+     def get_previous_pupil_size(self):
+          return self.previous_pupil_size
+
+     def set_previous_file_path(self, path):
+          self.previous_file_path = path
+     
+     def get_previous_file_path(self):
+          return self.previous_file_path
+     
+     def get_threshold(self):
+          return self.percentage_threshold
+     
+     def get_change_counter(self):
+          return self.change_counter
+
+     def increase_counter(self):
+          self.change_counter += 1
+
+     ####################################### 
+
      def convert_image(self,image_path):
           """
           Reads the image, write all the pupil pixels to 1 and everything else to 0
@@ -23,48 +53,54 @@ class PupilSizeCalculator:
           highlighted_image[~pupil_mask] = 0
           return highlighted_image, pupil_mask
      
-     def calculate_width_and_area(self,image_path):
+     def get_pupil_contour(self,recording):
+          """
+          Warning: the recording picture should show only the pupil
+          If some instruments' segmentation cover the pupil, the contour of the pupil can be in multiple pieces.
+          The function returns the (x,y,w,h) parameters for the merged contours.
+          """
+          contours, _ = cv2.findContours(recording.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+          if contours:
+               leftmost = (recording.shape[1], 0)
+               rightmost = (0, 0)
+               topmost = (0, recording.shape[0])
+               bottommost = (0, 0)
+
+               # Go through each contour to find the overall leftmost, rightmost, topmost, and bottommost points
+               for contour in contours:
+                    lm = tuple(contour[contour[:, :, 0].argmin()][0])
+                    rm = tuple(contour[contour[:, :, 0].argmax()][0])
+                    tm = tuple(contour[contour[:, :, 1].argmin()][0])
+                    bm = tuple(contour[contour[:, :, 1].argmax()][0])
+
+                    if lm[0] < leftmost[0]:
+                         leftmost = lm
+                    if rm[0] > rightmost[0]:
+                         rightmost = rm
+                    if tm[1] < topmost[1]:
+                         topmost = tm
+                    if bm[1] > bottommost[1]:
+                         bottommost = bm
+
+               width = rightmost[0] - leftmost[0]
+               height = bottommost[1] - topmost[1]
+
+               bounding_rect = (leftmost[0], topmost[1], width, height)
+          return bounding_rect
+
+     def calculate_width(self,image_path):
           """
           Calculates the width of the masked image based on the contour, and the difference of the most right and most left pixels.
-          Calculates the area of the masked image based on the contour.
           Returns nones if pupil is not found, or not the full pupil is detected on the image.
           """
           recording, pupil_mask = self.convert_image(image_path)
-
-          contours, _ = cv2.findContours(recording.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-          if contours:
-               largest_contour = max(contours, key=cv2.contourArea)
-
-               leftmost = tuple(largest_contour[largest_contour[:,:,0].argmin()][0])
-               rightmost = tuple(largest_contour[largest_contour[:,:,0].argmax()][0])
-               
-               width = rightmost[0] - leftmost[0]
-               area = cv2.contourArea(largest_contour)
-
+          x,y,w,h = self.get_pupil_contour(recording)
+          #if it is not near to a rounded shape
+          if w < 0.80*h or h < 0.80*w:
                # if the edge of the bounding box is exactly on the edge of the image shape, the pupil is probably not full on the image
-               x,y,w,h = cv2.boundingRect(largest_contour)
                if (x + w) == recording.shape[1] or (y + h) == recording.shape[0] or x == 0 or y == 0:
-                    return None, None
-               return width, area
-          else:
-               return None, None
-
-     def calculate_width_with_sum(self,image_path):
-          """
-          Calculates the width based on only summing the pixels in the row where there are the most pixels defined as pupil parts.
-          """
-          recording, mask = self.convert_image(image_path)
-          max_pixels_row = np.argmax(np.sum(mask, axis=1))
-          width_of_area_px = np.sum(mask[max_pixels_row])
-          return width_of_area_px
-     
-     def calculate_area(self, image_path):
-          """
-          Calculates the full area of the pupil (not instrument handling ready)
-          """
-          recording, mask = self.convert_image(image_path)
-          pupil_size = np.sum(mask)
-          return pupil_size
+                    return None
+          return w
 
      def save_pupil_mask(self, file_path, img_path):
           """
@@ -75,12 +111,10 @@ class PupilSizeCalculator:
           im[img == 0] = [0,0,0]
           im[img == 1] = [0,0,255]
 
-          #check bounding box
-          contours, _ = cv2.findContours(img.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-          if contours:
-               largest_contour = max(contours, key=cv2.contourArea)
-               x,y,w,h = cv2.boundingRect(largest_contour)
-               im = cv2.rectangle(im,(x,y),(x+w,y+h),(0,255,0),2)
+          x,y,w,h = self.get_pupil_contour(img)
+          top_left = (x, y)
+          bottom_right = (x + w, y + h)
+          cv2.rectangle(im, top_left, bottom_right, (0, 255, 0), 2)
 
           if not cv2.imwrite(file_path, im):
                print("Saving highlighted image was unsuccessful")
@@ -90,49 +124,50 @@ class PupilSizeCalculator:
           """
           Calculates the current width and compare it to the previous one.
           """
-          if self.previous_pupil_size is None:
-               self.previous_pupil_size = current_pupil_size
-               self.previous_file_path = image_path
+          previous_pupil_size = self.get_previous_pupil_size()
+          if previous_pupil_size is None:
+               self.set_previous_pupil_size(current_pupil_size)
+               self.set_previous_file_path(image_path)
                return False
-          change = abs(current_pupil_size - self.previous_pupil_size) / self.previous_pupil_size * 100
-          self.previous_pupil_size = current_pupil_size
-          if change > self.percentage_threshold:
-               #self.save_pupil_mask(f"pupil_change_{self.change_counter}_start.png", self.previous_file_path)
-               self.previous_file_path = image_path
-               #self.save_pupil_mask(f"pupil_change_{self.change_counter}_end.png", self.previous_file_path)
-               self.change_counter += 1
+          change = abs(current_pupil_size - previous_pupil_size) / previous_pupil_size * 100
+          self.set_previous_pupil_size(current_pupil_size)
+          if change > self.get_threshold():
+               #self.save_pupil_mask(f"pupil_change_{self.get_change_counter()}_start.png", self.get_previous_file_path())
+               self.set_previous_file_path(image_path)
+               #self.save_pupil_mask(f"pupil_change_{self.get_change_counter()}_end.png", self.get_previous_file_path())
+               self.increase_counter()
                return True
           return False
+
+
+def sort_key(filename):
+     """
+     sort files with regex based on the numbers in zfill() naming convention
+     """
+     match = re.search(r'frame(\d+)', filename)
+     return int(match.group(1)) if match else 0
+
 
 def process_folder(folder_path, threshold):
      print(f"Folder processing started...")
      calculator = PupilSizeCalculator(threshold)
-     for filename in os.listdir(folder_path):
+
+     sorted_filenames = sorted(os.listdir(folder_path), key=sort_key)
+
+     for filename in sorted_filenames:
           if filename.endswith(".png"):
                image_path = os.path.join(folder_path, filename)
-               pupil_width, pupil_area = calculator.calculate_width_and_area(image_path)
-               if pupil_width and pupil_area:
-                    print(f"Width of Pupil: {pupil_width}, area of pupil: {pupil_area} in file: {image_path}")
+               pupil_width= calculator.calculate_width(image_path)
+               if pupil_width:
+                    print(f"Width of Pupil: {pupil_width} in file: {image_path}")
                     if calculator.has_significant_change(pupil_width, image_path):
                          print(f"***** Significant width change detected in {filename} *****")
                else:
+                    calculator.reset_previous_pupil_size()
                     print(f"Unable to detect a full pupil on the recording {filename}.")
 
-process_folder("/home/data/CaDISv2/Video01/Labels", threshold=10)
+process_folder("/home/data/CaDISv2/Video03/Labels", threshold=10)
 
-"""
-# Find the row with the most pixels with the mask
-max_pixels_row = np.argmax(np.sum(pupil_mask, axis=1))
-width_of_area_px = np.sum(pupil_mask[max_pixels_row])
-
-# Find the column with the most pixels with the mask
-max_pixels_column = np.argmax(np.sum(pupil_mask, axis=0))
-height_of_area_px = np.sum(pupil_mask[:, max_pixels_column])
-
-#pupil mask area
-pupil_size = np.sum(pupil_mask)
-img_size = label_image.shape[0] * label_image.shape[1]
-"""
 
 
      
