086de04c2f2a92c4ca9c95132918d979a4e52854
MarVisT41 <97795525+MarVisT41@users.noreply.github.com>
2024-02-05 19:31:32 +0100

    tracking.py

--- src/tracking.py
+++ src/tracking.py
@@ -81,10 +81,11 @@ def tracking(tool_ind=0, path="/home/data/CaDISv2/Video01/Labels", output="/home
         M = cv2.moments(binary_image)
 
         # Calculate x, y coordinate of center
-        x = int(M["m10"] / M["m00"])
-        y = int(M["m01"] / M["m00"])
-        centroid_x[i] = x
-        centroid_y[i] = y
+        if moments['m00'] != 0:
+            x = int(M["m10"] / M["m00"])
+            y = int(M["m01"] / M["m00"])
+            centroid_x[i] = x
+            centroid_y[i] = y
 
         '''
         # Find contours in the binary mask
