19ecc8ab8b6875462da3b192984e2b646b7961b8
MarVisT41 <97795525+MarVisT41@users.noreply.github.com>
2024-02-21 14:43:41 +0100

    Update batch_segmentation.py

--- src/batch_segmentation.py
+++ src/batch_segmentation.py
@@ -1,12 +1,11 @@
 from transformers import AutoProcessor, AutoModelForUniversalSegmentation
 from torch.utils.data import DataLoader
-from cadis_dataset import CadisDataset
+from cadis_dataset import CadisDataset, CataractsDataset
 
 import argparse
 import torch
 import os
 from PIL import Image
-import numpy as np
 
 
 parser = argparse.ArgumentParser()
@@ -33,15 +32,11 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 model.to(device)
 
 for video_num in [2, 12, 22]:
-    test_dataset = CadisDataset(processor, video_numbers=[video_num])
+    test_dataset = CataractsDataset(processor, video_numbers=[video_num])
     test_dataloader = DataLoader(test_dataset, batch_size=1, shuffle=False)
 
     for i, (batch, original_image, ground_truth_labels) in enumerate(test_dataloader):
         batch = {k: v.to(device) for k, v in batch.items()}
-
-        # Drop the keys that are not accepted by the model in eval mode
-        keys_to_drop = ["mask_labels", "class_labels", "text_inputs"]
-        batch_ = {key: value for key, value in batch.items() if key not in keys_to_drop}
 
         with torch.no_grad():
             outputs = model(**batch_)
@@ -49,5 +44,5 @@ for video_num in [2, 12, 22]:
         # Postprocessing
         segmentation = processor.post_process_semantic_segmentation(outputs, target_sizes=[[540, 960]])[0]
         filename = os.path.join(args.output_dir, f'seg_{video_num}_{i}.png')
-        segmentation_image = Image.fromarray(segmentation.cpu().numpy().astype(np.uint8))
+        segmentation_image = Image.fromarray(segmentation.numpy())
         segmentation_image.save(filename)
