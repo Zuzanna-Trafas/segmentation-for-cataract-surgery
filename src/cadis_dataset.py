4ae22ac7154b5ed2bbfad51175c242247cac9e76
MarVisT41 <97795525+MarVisT41@users.noreply.github.com>
2024-02-21 14:44:03 +0100

    Update cadis_dataset.py
    Add CATARACT dataset class

--- src/cadis_dataset.py
+++ src/cadis_dataset.py
@@ -3,8 +3,10 @@ from PIL import Image
 import numpy as np
 import os
 import torch
+import matplotlib.image as img
+import re
 
-from constants import DATA_DIR
+from constants import DATA_DIR, CATARACTS_DATA_DIR
 
 
 class CadisDataset(Dataset):
@@ -108,6 +110,72 @@ class CadisDataset(Dataset):
                 inputs = {k:v.squeeze() if isinstance(v, torch.Tensor) else v[0] for k,v in inputs.items()}
 
                 return inputs, frame_image, label_image
+
+            idx -= len(video_data['frames'])
+
+    def __len__(self):
+        total_frames = sum(len(video_data['frames']) for video_data in self.video_metadata)
+        return total_frames
+
+
+class CataractsDataset(Dataset):
+    """
+    Custom dataset class for semantic segmentation.
+
+    Args:
+        processor: The processor for pre-processing images and labels.
+        video_numbers (list): List of video numbers to include in the dataset.
+        experiment (int): Experiment number for label remapping.
+    """
+    def __init__(self, processor, video_numbers=[2, 12, 22], experiment=None):
+        self.processor = processor
+        self.video_numbers = video_numbers
+        self.experiment = experiment
+        self.root_dir = CATARACTS_DATA_DIR
+        self.load_metadata()
+
+    def load_metadata(self):
+        """Load metadata for the dataset."""
+        self.video_metadata = []
+
+        for video_number in self.video_numbers:
+            formatted_video_number = f'{video_number:02d}'
+            video_data = {'video_number': formatted_video_number, 'frames': []}
+
+            images_path = os.path.join(self.root_dir, f'test{formatted_video_number}')
+
+            frame_filenames = sorted(os.listdir(images_path), key=self.natural_key)
+
+            for frame_filename in frame_filenames:
+                frame_path = os.path.join(images_path, frame_filename)
+
+                image = img.imread(frame_path)
+                if not((np.unique(image) == [0]).all()):
+                    if os.path.isfile(frame_path):
+                        video_data['frames'].append({
+                            'frame_path': frame_path
+                        })
+
+            self.video_metadata.append(video_data)
+
+    def natural_key(self, string_):
+        """See https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/"""
+        return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
+    
+    def __getitem__(self, idx):
+        """Return next frame by index"""
+        for video_data in self.video_metadata:
+            if idx < len(video_data['frames']):
+                frame_data = video_data['frames'][idx]
+                frame_path = frame_data['frame_path']
+
+                frame_image = np.array(Image.open(frame_path))
+
+                # Map the image and label to the form needed by the model
+                inputs = self.processor(images=frame_image, task_inputs=["panoptic"], return_tensors="pt")
+                inputs = {k:v.squeeze() if isinstance(v, torch.Tensor) else v[0] for k,v in inputs.items()}
+
+                return inputs, frame_image
 
             idx -= len(video_data['frames'])
 
