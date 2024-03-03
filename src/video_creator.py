f3846cbff48b32852cf9d8b842d48a8f959e47c6
MarVisT41 <97795525+MarVisT41@users.noreply.github.com>
2024-03-03 08:30:52 +0100

    Update video_creator.py
    Adjust sorted function

--- src/video_creator.py
+++ src/video_creator.py
@@ -4,6 +4,12 @@ import glob
 import os
 import sys
 import argparse
+import re
+
+
+def natural_key(string_):
+    """See https://blog.codinghorror.com/sorting-for-humans-natural-sort-order/"""
+    return [int(s) if s.isdigit() else s for s in re.split(r'(\d+)', string_)]
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -31,10 +37,9 @@ parser.add_argument(
 args = parser.parse_args()
  
 img_array = []
-path = os.path.join(args.input_folder, '*.png')
-image_files = sorted(glob.glob(path), key=lambda x: int(os.path.basename(x).split('_')[1]))
+image_files = sorted(os.listdir(args.input_folder), key=natural_key)
 for filename in image_files:
-    img = cv2.imread(filename)
+    img = cv2.imread(os.path.join(args.input_folder, filename))
     height, width, layers = img.shape
     size = (width,height)
     img_array.append(img)
@@ -45,3 +50,5 @@ out = cv2.VideoWriter(video_filepath,cv2.VideoWriter_fourcc(*'mp4v'), args.frame
 for i in range(len(img_array)):
     out.write(img_array[i])
 out.release()
+
+print("Finish!!")
